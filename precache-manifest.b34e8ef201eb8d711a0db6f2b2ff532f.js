3a5fed58d06c30bfa1c02416d4405760c75026b1
Farman hafeez <farmanhafeezj@gmail.com>
2020-08-07 20:57:22 +0530

    Updates

---
+++ precache-manifest.b34e8ef201eb8d711a0db6f2b2ff532f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f407f4b85a7a37e7e382c375979ff4",
+    "url": "/salary-converter/index.html"
+  },
+  {
+    "revision": "9f59f070610274d4262a",
+    "url": "/salary-converter/static/css/main.fe0651be.chunk.css"
+  },
+  {
+    "revision": "b49dd74438fb3cf35f34",
+    "url": "/salary-converter/static/js/2.9368ddfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/salary-converter/static/js/2.9368ddfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f59f070610274d4262a",
+    "url": "/salary-converter/static/js/main.01478fea.chunk.js"
+  },
+  {
+    "revision": "81412f7ba39677afc8ae",
+    "url": "/salary-converter/static/js/runtime-main.8446e875.js"
+  }
+]);
